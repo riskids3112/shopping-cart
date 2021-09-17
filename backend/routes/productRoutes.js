ae594d8b2e287e3f2ce774b78c74ca15c1f29895
riskids3112 <riskydeva13@gmail.com>
2021-09-17 14:12:55 +0700

    Update-17-09-2021

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const {getAllProducts, getProductById} = require("../controllers/productControllers");
+
+//@desc:  ini digunakan untuk mengambil seluruh product dari database
+//@route: GET //api/products
+//@access: Public
+router.get("/", getAllProducts);
+
+//@desc:  ini digunakan untuk mengambil satu product dari database berdasarkan id
+//@route: GET //api/products/:id
+//@access: Public
+router.get("/:id", getProductById);
+
+module.exports = router;
