c341ef4491485d2d5ccbb46e15e89caadedc537b
riskids3112 <riskydeva13@gmail.com>
2021-10-18 14:28:19 +0700

    update-18-oktober-2021

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,25 +1,57 @@
 import "./CartScreen.css";
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {Link} from "react-router-dom";
 
 //components
 import CartItem from "../components/CartItem";
 
+//Actions
+import {addToCart, removeFromCart} from "../redux/actions/cartActions";
+
 const CartScreen = () => {
+    const dispatch = useDispatch();
+    
+    const cart = useSelector((state) => state.cart);
+    const {cartItems} = cart;
+
+    const qtyChangeHandler = (id, qty) => {
+        dispatch(addToCart(id, qty));
+    };
+
+    const removeHandler = (id) => {
+        dispatch(removeFromCart(id));
+    };
+
+    const getCartCount = () => {
+        return cartItems.reduce((qty, item) => Number (item.qty) + qty, 0);
+    };
+
+    const getCartSubtotal = () => {
+        return cartItems.reduce((price, item) => item.price * item.qty + price, 0);
+    };
+
     return (
     <div className="cartscreen">
         <div className="cartscreen__left">
-            <h2>Your Cart</h2>
-
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
-            <CartItem />
+            <h2>Your Cart:</h2>
+            {cartItems.length === 0 ? (
+                <div>
+                    Your cart is empty <Link to="/">Continue Shopping</Link>
+                </div>
+            ) : ( 
+            cartItems.map((item) => (
+            <CartItem
+            key={item.product}
+            item={item} qtyChangeHandler={qtyChangeHandler}
+            removeHandler={removeHandler}/>
+            ))
+            )}
         </div>
     <div className="cartscreen__right">
         <div className="cartscreen__info">
-            <p>Sub Total (0) item</p>
-            <p>$99.99</p>
+            <p>Sub Total ({getCartCount()}) items</p>
+            <p>${getCartSubtotal().toFixed(2)}</p>
         </div>
         <div>
             <button>Proceed to checkout</button>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,36 +1,66 @@
 import "./ProductScreen.css";
+import {useState, useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
 
-const ProductScreen = () => {
+//actions
+import {getProductDetails} from "../redux/actions/productActions";
+import {addToCart} from "../redux/actions/cartActions";
+
+const ProductScreen = ({match, history}) => {
+const [qty, setQty] = useState(1);
+const dispatch = useDispatch();
+
+const productDetails = useSelector((state) => state.getProductDetails);
+const {loading, error, product} = productDetails;
+
+useEffect(() => {
+    if (product && match.params.id !== product._id) {
+        dispatch(getProductDetails(match.params.id));
+    }
+}, [dispatch, product, match]);
+
+const addToCartHandler = () => {
+    dispatch(addToCart(product._id, qty));
+    history.push("/cart");
+};
+
     return (
     <div className="productscreen">
+        {loading ? (
+        <h2>Loading...</h2> 
+        ) : error ? (
+        <h2>{error}</h2>
+        ) : (
+        <>
         <div className="productScreen__left">
         <div className="left__image">
-            <img src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80" alt="Product Name"/>
+            <img src={product.imageUrl} alt={product.name} />
         </div>
 
         <div className="left__info">
-            <p className="left__name">Product 1</p>
-            <p>Price: $99.9</p>
-            <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor</p>
+            <p className="left__name">{product.name}</p>
+            <p>Price: ${product.price}</p>
+            <p>Description: {product.description}</p>
         </div>
         </div>
         <div className="productScreen__right">
             <div className="right__info">
-                <p>Price: <span>$99.9</span></p>
-                <p>Status: <span>In Stock</span></p>
+                <p>Price: <span>${product.price}</span></p>
+                <p>Status:{" "} <span>{product.countInStock > 0 ? "In Stock" : "Out of Stock"}</span></p>
                 <p>Qty
-                <select>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
+                <select value={qty} onChange={(e) => setQty(e.target.value)}>
+                    {[...Array(product.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                        </option>
+                    ))}
                 </select>
                 </p>
-                <p><button type="button">Add To Cart</button></p>
+                <p><button type="button" onClick={addToCartHandler}>Add To Cart</button></p>
             </div>
         </div>
+        </>
+        )}
     </div>
     );
 };
